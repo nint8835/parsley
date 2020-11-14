7f371eafbfe3ae38661509a76fc63277650bd5bc
Riley Flynn <riley@rileyflynn.me>
2020-11-13 22:42:01 -0330

    Stop being main, remove main file

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/bwmarrin/discordgo"
-
-func main() {
-	parser := New(".")
-
-	parser.RunCommand(&discordgo.MessageCreate{Message: &discordgo.Message{Content: "."}})
-}
